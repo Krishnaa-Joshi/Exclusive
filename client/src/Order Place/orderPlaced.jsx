8cdd7349afed5b3b6702561326e0fe7931c1cfb9
Krishnaa-Joshi <krishnajos01@gmail.com>
2024-12-04 18:57:32 +0530

    wip

---
+++ client/src/Order Place/orderPlaced.jsx
@@ -0,0 +1,20 @@
+import HeroSection from "@/component/home component/Hero Section/hero";
+import NavBar from "@/component/common component/Navbar/navbar";
+import Footer from "@/component/common component/footer/footer";
+
+
+
+function OrderPlaced(){
+    return (
+        <>
+            <HeroSection />
+            <NavBar />
+
+            
+
+            <Footer/>
+        </>
+    )
+}
+
+export default OrderPlaced;
